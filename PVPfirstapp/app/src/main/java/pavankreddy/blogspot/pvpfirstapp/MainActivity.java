96004f866d01b4c888cdd78b65a656f6cee0ec7f
PAVAN KUMAR REDDY <pavankumart46@gmail.com>
2019-12-09 14:17:47 +0530

    Code Changes for PVP first app

--- PVPfirstapp/app/src/main/java/pavankreddy/blogspot/pvpfirstapp/MainActivity.java
+++ PVPfirstapp/app/src/main/java/pavankreddy/blogspot/pvpfirstapp/MainActivity.java
@@ -3,12 +3,35 @@ package pavankreddy.blogspot.pvpfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int count = 0;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Connect the text view on Xml file to textView object
+        textView = findViewById(R.id.count_tv);
+
+    }
+
+    public void showToast(View view)
+    {
+        Toast.makeText(this, "Hi, PVP IT ", Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    public void showCount(View view)
+    {
+        count++;
+        textView.setText(String.valueOf(count));
+        //textView.setText(""+count);
     }
 }
