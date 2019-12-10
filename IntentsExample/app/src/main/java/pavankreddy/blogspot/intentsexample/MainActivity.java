bac9a6f4d6b3ad578ae71e503ed5fe4ba0a83d20
PAVAN KUMAR REDDY <pavankumart46@gmail.com>
2019-12-10 10:29:37 +0530

    implicit intents

---
+++ IntentsExample/app/src/main/java/pavankreddy/blogspot/intentsexample/MainActivity.java
@@ -0,0 +1,38 @@
+package pavankreddy.blogspot.intentsexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openUrl(View view)
+    {
+        //Step-1: get the text from EditText box on the xml file
+        EditText et = findViewById(R.id.url_text_box);
+        String pvp = et.getText().toString();
+        // Step-2: Write implicit intent to open a web browser with the link
+        Intent i = new Intent();
+            // Sub step 1 - Set the action on Intent object
+        i.setAction(Intent.ACTION_VIEW);
+            // Sub Step 2 - Set the Data For the intent
+        i.setData(Uri.parse(pvp));
+
+        //Shorter Form of the above steps is
+        //Intent i = new Intent(Intent.ACTION_VIEW,Uri.parse(pvp));
+
+        // Step-3 Send the request to the system
+        startActivity(i);
+    }
+}
